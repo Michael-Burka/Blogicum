3a39ab520a5f34819ce3e2ffd114f9752c6305ae
Michael Burka <contact@michaelburka.com>
2023-05-31 16:36:47 +0400

    Connect file backend

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -133,3 +133,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+
+EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
