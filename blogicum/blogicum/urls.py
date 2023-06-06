f701f054c6f54c4db1859b7e644fd927bddaab3e
Michael Burka <contact@michaelburka.com>
2023-06-06 11:32:36 +0400

    Add MediaURL,MediaRoot

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.forms import UserCreationForm
+from django.conf.urls.static import static
 from django.views.generic.edit import CreateView
 from django.urls import include, path, reverse_lazy
 
@@ -22,4 +24,4 @@ urlpatterns = [
             ),
         name='registration',
     ),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
