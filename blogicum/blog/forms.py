4b35c06720ca60696c21e6248c13e3f17f7b7262
Michael Burka <contact@michaelburka.com>
2023-06-08 16:23:40 +0400

    Add CommentForm and change the PostForm name

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 
-from .models import Post
+from .models import Comment, Post
 
 
-class CreatePostForm(forms.ModelForm):
+class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = '__all__'
@@ -14,3 +14,9 @@ class CreatePostForm(forms.ModelForm):
             )
         }
         exclude = ('author',)
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('text',)
