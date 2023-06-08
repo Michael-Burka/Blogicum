a1bdabd32f34d2bfeda335dafeb55e9792c04e78
Michael Burka <contact@michaelburka.com>
2023-06-08 16:56:01 +0400

    Fix CSRF_FAILURE_VIEW

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -141,7 +141,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 
 EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
 
-CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
+CSRF_FAILURE_VIEW = 'pages.views.csrf_failure'
 
 LOGIN_REDIRECT_URL = 'blog:index'
 
