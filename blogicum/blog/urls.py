0bf7b6c46c6bd5c4f22a25a1943716cda537b25a
Michael Burka <contact@michaelburka.com>
2023-06-08 16:30:06 +0400

    Add comment URLs

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -10,11 +10,6 @@ urlpatterns = [
         name='index'
     ),
     path(
-        'create_post/',
-        views.PostCreateView.as_view(), 
-        name='create_post'
-    ),
-    path(
         'posts/<int:pk>/',
         views.PostDetailView.as_view(),
         name='post_detail'
@@ -33,5 +28,35 @@ urlpatterns = [
         'profile/<str:username>/edit/',
         views.ProfileUpdateView.as_view(),
         name='edit_profile'
+    ),
+    path(
+        'posts/create/',
+        views.PostCreateView.as_view(),
+        name='create_post'
+    ),
+    path(
+        'posts/<int:pk>/edit/',
+        views.PostEditUpdateView.as_view(),
+        name='edit_post'
+    ),
+    path(
+        'posts/<int:pk>/delete/',
+        views.PostDeleteView.as_view(),
+        name='delete_post'
+    ),
+    path(
+        'posts/<int:pk>/add_comment/',
+        views.add_comment,
+        name='add_comment'
+    ),
+    path(
+        'posts/<int:pk>/edit_comment/<int:comment_id>/',
+        views.CommentUpdateView.as_view(),
+        name='edit_comment'
+    ),
+    path(
+        'posts/<int:pk>/delete_comment/<int:comment_id>/',
+        views.CommentDeleteView.as_view(),
+        name='delete_comment'
     ),
 ]
